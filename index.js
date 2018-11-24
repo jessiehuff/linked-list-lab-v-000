fcb5bac2a74aa7ae1446d1f04f2028018588902f
Jessie Huff <huffjd1@gmail.com>
2018-11-24 18:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function next(node, collection){
   return collection[newAddress]
 }
 
+function nodeAt(node, linkedList, collection){
+  let currentNode = headNode(linkedList, collection)
+  for (let i=0; i<index; i++){
+    currentNode = next(currentNode, collection)
+  }
+  return currentNode
+}
+
 function indexAt(node, collection, linkedList) {
   let currentNode = headNode(linkedList, collection); 
   let currentIndex = 0 
