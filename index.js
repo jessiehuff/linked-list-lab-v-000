614e3404008e56291a9393b69002c3a83ae0d73e
Jessie Huff <huffjd1@gmail.com>
2018-11-24 18:28:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getName(node){
+  return node.name
+}
+
+function headNode(linkedList, collection){
+  return collection[linkedList]
+}
+
+function next(node){
+  let newAddress = node[1]
+  return collection[newAddress]
+}
+
+function indexAt(node, collection, linkedList) {
+  let currentNode = headNode(linkedList, collection); 
+  let currentIndex = 0 
+  while (currentNode != node){
+    currentIndex++
+    currentNode = next(currentNode, collection)
+  }
+  return currentIndex
+}
