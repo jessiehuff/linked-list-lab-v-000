103be585e1367377037f60648833fa12507d699c
Jessie Huff <huffjd1@gmail.com>
2018-11-24 18:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function indexAt(node, collection, linkedList) {
 
 function insertNodeAt(index, newNodeAddress, linkedList, collection){
   let previousNode = nodeAt(index-1, linkedList, collection)
-  let nextNode = nodeAt(index, linkedList, collection
+  let nextNode = nodeAt(index, linkedList, collection)
   
   let previousNodeIndex = indexAt(previousNode, collection, linkedList)
   let nextNodeIndex = indexAt(nextNode, collection, linkedList)
