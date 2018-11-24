3f39e0b81afa0ed990e5d3f32d3c8441eceda4c6
Jessie Huff <huffjd1@gmail.com>
2018-11-24 18:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function addressAt(index, linkedList, collection){
   if (index === 0){
     return linkedList
   } else {
-    let node = nodeAt(index, linkedList, collection)
+    let node = nodeAt(index-1, linkedList, collection)
     return node.next
   }
 }
