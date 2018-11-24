a2063a80d853a193c1d26d7cd8f0f06ebbf04c2b
Jessie Huff <huffjd1@gmail.com>
2018-11-24 18:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function next(node, collection){
   return collection[newAddress]
 }
 
-// function indexAt(node, collection, linkedList) {
-//   let currentNode = headNode(linkedList, collection); 
-//   let currentIndex = 0 
-//   while (currentNode != node){
-//     currentIndex++
-//     currentNode = next(currentNode, collection)
-//   }
-//   return currentIndex
-// }
+function indexAt(node, collection, linkedList) {
+  let currentNode = headNode(linkedList, collection); 
+  let currentIndex = 0 
+  while (currentNode != node){
+    currentIndex++
+    currentNode = next(currentNode, collection)
+  }
+  return currentIndex
+}
